1ef165b39841be3e8c5a198e2ea97c17e0b1107c
sillypears <sillypears@users.noreply.github.com>
2023-04-02 09:39:27 -0400

    Oke

--- routes/root.js
+++ routes/root.js
@@ -17,19 +17,19 @@ const routes = [
         method: 'GET',
         url: '/api/getRandom/:max/:num',
         handler: rootController.getRandomNumber
-    },{
+    }, {
         method: 'GET',
         url: '/api/getUser/:id',
         handler: rootController.getRaffleUser
-    },{
+    }, {
         method: 'POST',
         url: '/api/createRaffle/',
         handler: rootController.createRaffle
-    },{
+    }, {
         method: 'POST',
         url: '/api/createUser/',
         handler: rootController.createUser
-    },{
+    }, {
         method: 'POST',
         url: '/api/saveRaffleWinners',
         handler: rootController.saveRaffleWinners
